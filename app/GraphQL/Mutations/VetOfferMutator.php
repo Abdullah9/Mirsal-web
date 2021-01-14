9d2c608434905eeb170f098e435baffc5f030681
roneilbrews <roneilsullano@gmail.com>
2021-01-14 18:49:04 +0800

    added downpayment  set up for vet offer payment

--- app/GraphQL/Mutations/VetOfferMutator.php
+++ app/GraphQL/Mutations/VetOfferMutator.php
@@ -2,18 +2,15 @@
 
 namespace App\GraphQL\Mutations;
 
-use GraphQL\Type\Definition\ResolveInfo;
-use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use App\Model\AdminSetting;
+use App\Model\Invoice;
 use App\Model\VetOffer;
 use App\Model\VetRequest;
-use App\Model\Invoice;
 use App\Model\VetTimeSlot;
-use App\Model\AdminSetting;
 
 class VetOfferMutator
 {
 
-    
     public function acceptVetOffer($root, array $args)
     {
         $vet_offer_id = $args['vet_offer_id'];
@@ -23,53 +20,68 @@ class VetOfferMutator
         $vet_request = VetRequest::find($vet_offer->vet_request_id);
         $vetTimeSlotQuery = VetTimeSlot::query();
         // echo json_encode($vet_request); exit;
-        if($vet_request->type == "VISIT" && $vet_request->vet_time_slot_id != NULL){
+        if ($vet_request->type == "VISIT" && $vet_request->vet_time_slot_id != null) {
             $vetTimeSlot = $vetTimeSlotQuery->find($vet_request->vet_time_slot_id);
-            if($vetTimeSlot->taken == 1){
+            if ($vetTimeSlot->taken == 1) {
                 return array(
                     'status' => 0,
-                    'message' => __('lang.time_slot_taken')
+                    'message' => __('lang.time_slot_taken'),
                 );
             } else {
                 $vetTimeSlot->taken = 1;
                 $vetTimeSlot->save();
             }
         }
-        
-        if(isset($vet_offer_id)){
+
+        if (isset($vet_offer_id)) {
 
             // $vet_offer->status = "ACCEPTED";
             // $vet_request->status = "ACCEPTED";
-            $existingInvoice = Invoice::where('vet_offer_id', $vet_offer_id)->where('payment_for','VETERINARIAN')->get();
-        
-            if($existingInvoice->isEmpty()){
-                
-                $invoice = new Invoice;
-                $invoice->client_id = $vet_offer->vetRequest->client_id;
-                $invoice->vet_offer_id = $vet_offer_id;
-                // $invoice->offer_id = $vet_offer_id;
-                $invoice->payment_for = "VETERINARIAN";
+            $existingInvoice = Invoice::where('vet_offer_id', $vet_offer_id)->where('payment_for', 'VETERINARIAN')->get();
+
+            if ($existingInvoice->isEmpty()) {
+
                 $price = str_replace(',', "", $vet_offer->price);
                 //Amount calculation
-                // $invoice->amount_paid = $price; 
-
-                $admin_commission = $price  * ($admin_setting->admin_commission_perc/100);
+                // $invoice->amount_paid = $price;
+                if ($vet_request->type == "VISIT") {
+                    $admin_commission = $price * ($admin_setting->admin_commission_perc / 100);
+                } else {
+                    $admin_commission = $price * ($admin_setting->first_payment_perc / 100);
+                }
                 $payable_amount = $price - $admin_commission;
 
-                $amount_tax_price = $payable_amount * ($admin_setting->tax_perc/100);
-                $commission_tax_price = $admin_commission * ($admin_setting->tax_perc/100);
+                $amount_tax_price = $payable_amount * ($admin_setting->tax_perc / 100);
+                $commission_tax_price = $admin_commission * ($admin_setting->tax_perc / 100);
 
                 $payable_amount_with_tax = $payable_amount + $amount_tax_price;
                 $admin_commission_with_tax = $admin_commission + $commission_tax_price;
 
-                $tax_percentage = $admin_setting->tax_perc/100;
+                $tax_percentage = $admin_setting->tax_perc / 100;
+
+                $payable_amount = $price - $admin_commission;
+
+                $amount_tax_price = $payable_amount * ($admin_setting->tax_perc / 100);
+                $commission_tax_price = $admin_commission * ($admin_setting->tax_perc / 100);
+
+                $payable_amount_with_tax = $payable_amount + $amount_tax_price;
+                $admin_commission_with_tax = $admin_commission + $commission_tax_price;
+
+                $tax_percentage = $admin_setting->tax_perc / 100;
+
+                $invoice = new Invoice;
+                $invoice->client_id = $vet_offer->vetRequest->client_id;
+                $invoice->vet_offer_id = $vet_offer_id;
+
+                $invoice->payment_for = "VETERINARIAN";
+
                 $invoice->amount_paid = $payable_amount_with_tax + $admin_commission_with_tax;
-                
+
                 $invoice->tax_price = $amount_tax_price + $commission_tax_price;
                 $invoice->tax_rate = $admin_setting->tax_perc;
                 $vet_offer->tax_price = $amount_tax_price;
                 $vet_offer->total = $payable_amount_with_tax + $admin_commission_with_tax;
-                
+
                 $invoice->service_provider_id = $vet_offer->vet_id;
                 $invoice->provider_profit = $payable_amount_with_tax;
                 $invoice->admin_commission = $admin_commission_with_tax;
@@ -81,18 +93,17 @@ class VetOfferMutator
                 return array(
                     'status' => 1,
                     'message' => "Success",
-                    'invoice' => $invoice
+                    'invoice' => $invoice,
                 );
             } else {
 
                 return array(
                     'status' => 1,
                     'message' => "Success",
-                    'invoice' => $existingInvoice->first()
+                    'invoice' => $existingInvoice->first(),
                 );
             }
 
-            
         }
 
         return array(
@@ -100,9 +111,7 @@ class VetOfferMutator
             'message' => "Failed",
 
         );
-        
 
     }
-
 
 }
